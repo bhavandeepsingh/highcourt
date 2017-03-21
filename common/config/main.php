51ad2d38be452c31482730e9f716500122551616
bhavandeepboparai <ghudani1@gmail.com>
2017-03-21 12:24:31 +0530

    stable

--- common/config/main.php
+++ common/config/main.php
@@ -5,5 +5,17 @@ return [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
+        'user' => [    
+            //'class' => 'common\models\User',
+            'identityClass' => 'common\models\User',
+            'loginUrl' => ['site/login'],
+        ],
+        'as access' => [
+            'class' => 'mdm\admin\components\AccessControl',
+            'allowActions' => [
+                'site/*',
+                'admin/*',
+            ]
+        ]
     ],
 ];
