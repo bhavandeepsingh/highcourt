d468b2b403730b67e60c31e05feba5359e6ece18
bhavandeepsingh <ghudani1@gmail.com>
2017-04-19 13:26:59 +0530

    api module add

--- api/config/main.php
+++ api/config/main.php
@@ -7,45 +7,11 @@ $params = array_merge(
 );
 
 return [
-    'id' => 'app-backend',
+    'id' => 'api',
     'basePath' => dirname(__DIR__),
-    'controllerNamespace' => 'backend\controllers',
+    'controllerNamespace' => 'api\controllers',
     'bootstrap' => ['log'],
-    'modules' => [
-        'as backend' => 'dektrium\user\filters\BackendFilter',
-    ],
     'components' => [
-        'request' => [
-            'csrfParam' => '_csrf-backend',
-        ],
-        'user' => [
-            //'identityClass' => 'common\models\User',
-            //'enableAutoLogin' => true,
-            //'identityCookie' => ['name' => '_identity-backend', 'httpOnly' => true],
-            'identityCookie' => [
-                'name'     => '_backendIdentity',
-                'path'     => 'backend/',
-                'httpOnly' => true,
-            ],
-        ],
-        'view' => [
-            'theme' => [
-                'pathMap' => [
-                    '@dektrium/user/views' => '@app/views/user'
-                ],
-            ],
-        ],
-        'session' => [
-            'name' => 'BACKENDSESSID',
-            'cookieParams' => [
-                'httpOnly' => true,
-                'path'     => 'backend/',
-            ],
-        ],
-        /*'session' => [
-            // this is the name of the session cookie used for login on the backend
-            'name' => 'advanced-backend',
-        ],*/
         'log' => [
             'traceLevel' => YII_DEBUG ? 3 : 0,
             'targets' => [
@@ -57,28 +23,6 @@ return [
         ],
         'errorHandler' => [
             'errorAction' => 'site/error',
-        ],
-        /*
-        'urlManager' => [
-            'enablePrettyUrl' => true,
-            'showScriptName' => false,
-            'rules' => [
-            ],
-        ],
-        */
-    ],
-    'modules' => [
-        'user' => [
-            // following line will restrict access to admin controller from backend application
-            'as backend' => 'dektrium\user\filters\BackendFilter',
-            'enableUnconfirmedLogin' => true,
-            'confirmWithin' => 21600,
-            'cost' => 12,
-            'admins' => ['admin'],
-            'modelMap' => [
-                'User' => 'common\models\User',
-                'Profile' => 'common\models\Profile',
-            ],
         ],
     ],
     'params' => $params,
